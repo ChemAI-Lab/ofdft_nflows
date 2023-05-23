591eb082a777c370b5d6fd40a3eb89d87b0beb56
RodrigoAVargasHdz <32406514+RodrigoAVargasHdz@users.noreply.github.com>
2023-05-23 18:18:40 -0400

    norm flow ho 1d

--- harmonic_oscillator_1d.py
+++ harmonic_oscillator_1d.py
@@ -1,45 +1,112 @@
-from typing import Any, Union, Callable
+from typing import Any, Callable, Sequence, Optional, Union
+from functools import partial
+
 import jax
 import jax.numpy as jnp
-from jax import jit
+from jax import vmap, jit
 import jax.random as jrnd
+from jax._src import prng
+
+import flax
+from flax import linen as nn
+import optax
 
 from ofdft_normflows.functionals import laplacian
+from ofdft_normflows.flows import NormFlow
 
 Array = Any
+KeyArray = Union[Array, prng.PRNGKeyArray]
 
 
 @jit
-def uniform_pdf(x: Any, a: Any = -10., b: Any = 10.0) -> jnp.DeviceArray:
+def uniform_pdf(x: Any, a: Any = -5., b: Any = 5.0) -> jnp.DeviceArray:
     dx = 1./(b-a)
     return dx*jnp.ones_like(x)
 
 
-@jit
-def potential_energy(x: Any, k: Any = 10.) -> jnp.DeviceArray:
-    return jnp.sum(k*x**2)
+# @jit
+def potential_energy(params: Any, u: Any, T: Callable, k: Any = 10.) -> jnp.DeviceArray:
+    x, _ = T(params, u)
+    return 0.5*k*jnp.mean(x**2)
 
 
-def kinetic_energy(x: Any, params: Any, rho: Callable) -> jnp.DeviceArray:
-    def sqrt_rho(x, params): return jnp.sqrt(rho(params, x))  # flax format
-    lap_val = laplacian(x, params, sqrt_rho)
-    rho_val = rho(params, x)
+def kinetic_energy(params: Any, u: Any, rho: Callable) -> jnp.DeviceArray:
+    def sqrt_rho(u, params): return jnp.sqrt(rho(params, u))  # flax format
+    lap_val = laplacian(u, params, sqrt_rho)
+    rho_val = rho(params, u)
     rho_val = 1./jnp.sqrt(rho_val)
-    return jnp.multiply(rho_val, lap_val)
+    return -0.5*jnp.mean(jnp.multiply(rho_val, lap_val))
 
 
 def main():
     png = jrnd.PRNGKey(0)
     _, key = jrnd.split(png)
-    u = jrnd.uniform(key, (10, 1), minval=-10., maxval=10.)
-    print(u)
-    print(uniform_pdf(u, -10., 10.))
-    def rho(x, params): return jax.scipy.stats.norm.pdf(x)
-    lap = laplacian(u, None, rho)
-    print(lap)
+    model = NormFlow(3, 1)
+    params0 = model.init(key, jnp.zeros((1, 1)))
+    z, ldj = model.apply(params0, jrnd.uniform(key, (2, 1)))
 
-    # x = jnp.zeros((10, 1))
-    # print(uniform_pdf(x, -1., 1.))
+    def log_density_forward(params, u):
+        x, log_det = model.apply(params, u)
+        log_p_u = jnp.log(uniform_pdf(u))
+        log_p_x = log_p_u - log_det
+        return log_p_x, x
+
+    def rho(params, u):
+        log_p_x, _ = log_density_forward(params, u)
+        return jnp.exp(log_p_x)
+
+    def fit(params: optax.Params,
+            optimizer: optax.GradientTransformation,
+            key: KeyArray,
+            epochs: int = 50000,
+            batch_size: int = 512) -> optax.Params:
+
+        opt_state = optimizer.init(params)
+
+        def loss(params, u_samples):
+            # log_p_x, x = log_density_forward(params, u_samples)
+            # potential energy
+            v = potential_energy(params, u_samples, model.apply)
+            t = kinetic_energy(params, u_samples, rho)
+            return t + v
+
+        @jax.jit
+        def step(params, opt_state, batch):
+            loss_value, grads = jax.value_and_grad(loss)(params, batch)
+            updates, opt_state = optimizer.update(grads, opt_state, params)
+            params = optax.apply_updates(params, updates)
+            return params, opt_state, loss_value
+
+        loss0 = jnp.inf
+        for i in range(epochs):
+            _, key = jrnd.split(key)
+
+            u_samples = jrnd.uniform(key, shape=(
+                batch_size, 1), minval=-5., maxval=5.)
+
+            params, opt_state, loss_value = step(params, opt_state, u_samples)
+            if loss_value < loss0 and loss_value > 0.:
+                loss0 = loss_value
+                params_opt = params
+            if i % 250 == 0:
+                print(f'step {i}, loss: {loss_value}')
+
+        return params_opt
+
+    optimizer = optax.adam(learning_rate=1e-3)
+    _, key = jrnd.split(key)
+    params = fit(params0, optimizer, key)
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    u = jnp.linspace(-5., 5., 1000)
+    u = jnp.expand_dims(u, axis=1)
+    rho_x = rho(params, u)
+    plt.plot(u, rho(params0, u), label='Rho init')
+    plt.plot(u, rho_x, label='Rho opt')
+    plt.legend()
+    plt.savefig('Figures/HO1D.png')
 
 
 if __name__ == "__main__":
--- ofdft_normflows/flows.py
+++ ofdft_normflows/flows.py
@@ -1,31 +1,48 @@
-from typing import Any, Callable, Sequence, Optional, Union
+from typing import Any, Callable, Sequence, Optional, Union, Tuple
 from functools import partial
 
 import jax
 import jax.numpy as jnp
-from jax import vmap
+from jax import vmap, lax
 import jax.random as jrnd
 from jax._src import prng
 
 import flax
 from flax import linen as nn
 from flax.linen import jit as jit_flax
+from flax.linen import initializers
+from flax.linen.module import compact, Module
+
 from jaxopt import ScipyRootFinding
 import optax
 
 Array = Any
 KeyArray = Union[Array, prng.PRNGKeyArray]
+PRNGKey = Any
+Shape = Tuple[int, ...]
+Dtype = Any  # this could be a real type?
+Array = Any
+PrecisionLike = Union[None, str, lax.Precision, Tuple[str, str],
+                      Tuple[lax.Precision, lax.Precision]]
+DotGeneralT = Callable[..., Array]
+ConvGeneralDilatedT = Callable[..., Array]
 
+default_kernel_init = initializers.lecun_normal()
+normal_init = initializers.normal(stddev=1.0)
+ones_init = initializers.ones
 
 # @jit_flax
-class PlanarFlow(nn.Module):
+
+
+class PlanarFlow_OLD(nn.Module):
     dims: int
     inv_num_iterations: int = 1000
 
     def setup(self):
         # we automatically know what to do with lists, dicts of submodules
-        self.linear0 = nn.Dense(1)
-        self.linear1 = nn.Dense(self.dims, use_bias=False)
+        self.linear0 = nn.Dense(1, kernel_init=jax.ones_init)
+        self.linear1 = nn.Dense(self.dims, use_bias=False,
+                                kernel_init=jax.ones_init)
 
     def _f(self, z):
         y = self.linear0(z)
@@ -64,24 +81,101 @@ class PlanarFlow(nn.Module):
             return y, log_det
 
 
+class PlanarFlow(nn.Module):
+    # https://github.com/VincentStimper/normalizing-flows/blob/master/normflows/flows/planar.py
+    features: int
+    x_features: int
+    kernel_init: Callable[[PRNGKey, Shape, Dtype],
+                          Array] = initializers.ones_init()   # default_kernel_init
+    bias_init: Callable[[PRNGKey, Shape, Dtype],
+                        Array] = initializers.zeros_init()
+    param_dtype: Dtype = jnp.float32
+    precision: PrecisionLike = None
+
+    def setup(self) -> None:
+        self.w = self.param('w',
+                            self.kernel_init,
+                            (self.x_features, 1),
+                            self.param_dtype)
+        self.b = self.param('b', self.bias_init, (1,),
+                            self.param_dtype)
+        self.u = self.param('u',
+                            self.kernel_init,
+                            (1, self.features),
+                            self.param_dtype)
+        self.act = nn.tanh
+
+    def _forward0(self, z: Any):
+        lin = lax.dot_general(z, self.w, (((z.ndim - 1,), (0,)), ((), ())),
+                              precision=self.precision,) + self.b
+        inner = jnp.sum(self.w*self.u)
+        u = self.u + (jnp.log(1 + jnp.exp(inner)) - 1 - inner) \
+            * self.w / jnp.sum(self.w ** 2)  # constraint w.T * u > -1
+
+        z_ = z + lax.dot_general(self.act(lin), self.u, (((lin.ndim - 1,), (0,)), ((), ())),
+                                 precision=self.precision,)
+        return z_
+
+    def _forward(self, z):
+        lin = lax.dot_general(z, self.w, (((z.ndim - 1,), (0,)), ((), ())),
+                              precision=self.precision,) + self.b
+        return z + lax.dot_general(self.act(lin), self.u, (((lin.ndim - 1,), (0,)), ((), ())),
+                                   precision=self.precision,)
+
+    def _log_det_forward(self, z: Any):
+        df_dz = jax.jacrev(self._forward)(z)
+        df_dz = df_dz.reshape(z.shape[-1], z.shape[-1])
+        v_det = jnp.abs(jax.numpy.linalg.det(df_dz))
+        return jnp.log(v_det)
+
+    def _inverse(self, x):
+        z = x
+        lin = lax.dot_general(z, self.w, (((z.ndim - 1,), (0,)), ((), ())),
+                              precision=self.precision,) + self.b
+
+    @compact
+    def __call__(self, z) -> Any:
+        x = self._forward(z)
+        log_det = vmap(self._log_det_forward)(jnp.expand_dims(z, axis=1))
+        return x, jnp.expand_dims(log_det, axis=1)
+
+
 # @jit_flax
 class NormFlow(nn.Module):
     n_flows: int
     dims: int
+    x_features: int = 1
+    inv_num_iterations: int = 100
     # N: int
 
     def setup(self):
-        self.flows = [PlanarFlow(self.dims)
+        self.flows = [PlanarFlow(self.dims, self.x_features)
                       for i in range(self.n_flows)]
 
-    def __call__(self, z, forward=True):
+    def _forward(self, z):
         sum_log_det = 0
         x = z  # transformed_sample
         for flow in self.flows:
-            z, ldj = flow(x, forward)
+            z, ldj = flow(x)
             x = z
             sum_log_det += ldj
         return x, sum_log_det
+
+    def _inverse(self, f_z):
+        def _f_root(z, f_z):
+            z, _ = self._forward(z)
+            return z - f_z
+        roots = ScipyRootFinding(method='hybr',
+                                 optimality_fun=_f_root)
+
+        return roots.run(init_params=jnp.zeros_like(f_z), f_z=f_z).params
+
+    @compact
+    def __call__(self, z, forward=True):
+        if forward:
+            return self._forward(z)
+        else:
+            return self._inverse(z)
 
 
 def main_single_flow():
@@ -97,19 +191,50 @@ def main_single_flow():
     print(w)
 
 
+def main_single_flow_ravh():
+    import jax.random as jrnd
+    rng = jrnd.PRNGKey(0)
+    _, key = jrnd.split(rng)
+    model = PlanarFlow_OLD(1)
+    params = model.init(key, jnp.ones((1, 1)))
+    print(params)
+    x, ldj = model.apply(params, jnp.ones((3, 1)))
+    print(x)
+
+    print('ravh')
+    model = PlanarFlow(1, 1)
+    params1 = model.init(key, jnp.ones((1, 1)))
+    print(params1)
+    x, ldj = model.apply(params1, jnp.ones((3, 1)))
+    print(x)
+    print(ldj)
+
+    zz1, _ = jax.tree_util.tree_flatten(params1)
+    for zi in enumerate(zz):
+        print(zi)
+    zz, _ = jax.tree_util.tree_flatten(params)
+    for zi in enumerate(zip(zz, zz1)):
+        print(zi)
+
+
 def main_flows():
     import jax.random as jrnd
     rng = jrnd.PRNGKey(0)
     _, key = jrnd.split(rng)
-    model = NormFlow(1, 1)
-    params = model.init(key, jnp.ones((3, 1)))
-    print(params)
-    x, ldj = model.apply(params, jnp.ones((3, 1)))
-    w = model.apply(params, x, False)
-    print(x)
-    print(w)
+    model = NormFlow(2, 1)
+    params = model.init(key, jnp.ones((1, 1)))
+    z = jrnd.normal(key, (10, 1))
+    x, ldj = model.apply(params, z)
+    z = model.apply(params, x, False)
+
+    print('NormFlow')
+    # print(params)
+    print('z', z)
+    print('forward', x)
+    print('reverse', z)
 
 
 if __name__ == "__main__":
     # main_single_flow()
+    # main_single_flow_ravh()
     main_flows()
