18e7090864bffd939ac9fef1497ea585fdfded57
RodrigoAVargasHdz <32406514+RodrigoAVargasHdz@users.noreply.github.com>
2023-07-20 16:11:49 -0400

    loss + cusp condition

--- h_atom_ofdft_min.py
+++ h_atom_ofdft_min.py
@@ -10,9 +10,9 @@ from jax._src import prng
 
 from flax.training import checkpoints
 import optax
-from distrax import MultivariateNormalDiag
+from distrax import MultivariateNormalDiag, Laplace
 
-from ofdft_normflows.functionals import _kinetic, Dirac_exchange
+from ofdft_normflows.functionals import _kinetic, Dirac_exchange, cusp_condition
 from ofdft_normflows.functionals import Hartree_potential, Nuclei_potential
 from ofdft_normflows.cn_flows import neural_ode
 from ofdft_normflows.cn_flows import Gen_CNFSimpleMLP as CNF
@@ -66,8 +66,8 @@ def get_scheduler(epochs: int, sched_type: str = 'zero'):
 
 def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = False, scheduler_type: str = 'ones'):
 
-    mol_name = 'H2'
-    n_particles = 2
+    mol_name = 'H'
+    n_particles = 1
     coords = jnp.array([[0., 0., 0.]])
     z = jnp.array([[1.]])
     # atoms = ['H']
@@ -97,6 +97,7 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
     mean = jnp.zeros((3,))
     cov = 0.1 * jnp.ones((3,))
     prior_dist = MultivariateNormalDiag(mean, cov)
+    # prior_dist = Laplace()
 
     # optimizer = optax.adam(learning_rate=1E-3)
     optimizer = optax.chain(
@@ -119,6 +120,13 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
         return jnp.exp(logp_zt)  # logp_x
 
     @jax.jit
+    def rho_rev(params, x):
+        zt = lax.concatenate((x, jnp.zeros((x.shape[0], 1))), 1)
+        z0, logp_z0 = NODE_rev(params, zt)
+        logp_x = prior_dist.log_prob(z0)[:, None] + logp_z0
+        return jnp.exp(logp_x)  # logp_x
+
+    @jax.jit
     def T(params, samples):
         zt, _ = NODE_fwd(params, samples)
         return zt
@@ -133,12 +141,15 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
         nuc_v = (n_particles)*v_functional(params, u_samples, T, mol)
         e_xc = (n_particles**(4/3))*Dirac_exchange(params, u_samples, rho)
 
-        e = t + nuc_v + ci*c_v + e_xc
+        cusp = cusp_condition(params, rho_rev, mol)
+
+        e = t + nuc_v + ci*c_v + e_xc + cusp
         return jnp.mean(e), {"t": jnp.mean(t),
                              "v": jnp.mean(nuc_v),
                              "h": jnp.mean(c_v),
                              "x": jnp.mean(e_xc),
-                             "e": jnp.mean(t + nuc_v + c_v + e_xc)}
+                             "e": jnp.mean(t + nuc_v + c_v + e_xc),
+                             "cusp": cusp}
 
     @jax.jit
     def step(params, opt_state, batch, ci):
@@ -188,16 +199,16 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
         mean_energy = losses['e']
         r_ = {'epoch': i,
               'L': loss_epoch, 'E': mean_energy,
-              'T': losses['t'], 'V': losses['v'], 'H': losses['h'], 'X': losses['x'],
+              'T': losses['t'], 'V': losses['v'], 'H': losses['h'], 'X': losses['x'], 'cusp': losses['cusp'],
               #   'I': norm_integral, 'ci': ci
               }
         df = pd.concat([df, pd.DataFrame(r_, index=[0])], ignore_index=True)
         df.to_csv(
-            f"{CKPT_DIR}/training_trajectory_Ne_{n_particles}.csv", index=False)
+            f"{CKPT_DIR}/training_trajectory_{mol_name}_{n_particles}.csv", index=False)
 
         if i % 5 == 0:
             _s = f"step {i}, L: {loss_epoch:.3f}, E:{mean_energy:.3f}\
-            T: {losses['t']:.5f}, V: {losses['v']:.5f}, H: {losses['h']:.5f}, X: {losses['x']:.5f}"
+            T: {losses['t']:.5f}, V: {losses['v']:.5f}, H: {losses['h']:.5f}, X: {losses['x']:.5f}, cusp: {losses['cusp']:.5f}"
             print(_s,
                   file=open(f"{CKPT_DIR}/loss_epochs_{mol_name}.txt", 'a'))
 
@@ -208,7 +219,7 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
             #     ckpt_dir=CKPT_DIR, target=params, step=0, overwrite=True)
 
         if i % 20 == 0 or i < 25:
-            xt = jnp.linspace(-2.5, 2.5, 1000)
+            xt = jnp.linspace(-4.5, 4.5, 1000)
             yz = jnp.zeros((xt.shape[0], 2))
             zt = lax.concatenate((xt[:, None], yz), 1)
             zt_and_logp_zt = lax.concatenate(
@@ -237,20 +248,21 @@ def training(batch_size: int = 256, epochs: int = 100, bool_load_params: bool = 
             plt.plot(xt, n_particles*jnp.exp(rho_pred),
                      color='tab:blue', label=r'$N_{e}\;\rho_{NF}(x)$')
             # plt.plot(xt, v_pot,
-            #          ls='--', color='k', label=r'$V(x)$')
+            #  ls='--', color='k', label=r'$V(x)$')
             plt.xlabel('x [Bhor]')
+            plt.ylim(-10, 2.1)
             # plt.ylabel('Energy units')
             plt.legend()
             plt.tight_layout()
             plt.savefig(f'{FIG_DIR}/rho_and_V_pot_{i}.png')
 
-            assert 0
+            # assert 0
 
 
 def main():
     parser = argparse.ArgumentParser(description="Density fitting training")
     parser.add_argument("--epochs", type=int,
-                        default=100, help="training epochs")
+                        default=50, help="training epochs")
     parser.add_argument("--bs", type=int, default=12, help="batch size")
     parser.add_argument("--params", type=bool, default=False,
                         help="load pre-trained model")
