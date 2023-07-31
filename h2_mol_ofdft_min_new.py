878cc766e3aab32c8671506f62ed4200958fd306
RodrigoAVargasHdz <32406514+RodrigoAVargasHdz@users.noreply.github.com>
2023-07-30 22:58:47 -0300

    den and x before functionals

---
+++ h2_mol_ofdft_min_new.py
@@ -0,0 +1,297 @@
+import os
+import argparse
+from functools import partial
+from typing import Any, Union
+import pandas as pd
+
+import jax
+from jax import lax, vmap, numpy as jnp
+import jax.random as jrnd
+from jax._src import prng
+
+from flax.training import checkpoints
+import optax
+from distrax import MultivariateNormalDiag, Laplace
+
+from ofdft_normflows.functionals_new import _kinetic, _nuclear, _hartree, _exchange, cusp_condition
+from ofdft_normflows.dft_distrax import DFTDistribution
+from ofdft_normflows.cn_flows import neural_ode
+from ofdft_normflows.cn_flows import Gen_CNFSimpleMLP as CNF
+from ofdft_normflows.cn_flows import Gen_CNFRicky as CNFRicky
+from ofdft_normflows.utils import get_scheduler
+
+import matplotlib.pyplot as plt
+
+Array = Any
+KeyArray = Union[Array, prng.PRNGKeyArray]
+
+jax.config.update("jax_enable_x64", True)
+# jax.config.update('jax_disable_jit', True)
+
+BHOR = 1.8897259886  # 1AA to BHOR
+# CKPT_DIR = "Results/GP_pot"
+# FIG_DIR = "Figures/GP_pot"
+
+
+@ partial(jax.jit,  static_argnums=(2,))
+def compute_integral(params: Any, grid_array: Any, rho: Any, Ne: int, bs: int):
+    grid_coords, grid_weights = grid_array
+    rho_val = Ne*rho(params, grid_coords)
+    return jnp.vdot(grid_weights, rho_val)
+
+
+def training(t_kin: str = 'TF',
+             v_pot: str = 'HGH',
+             h_pot: str = 'MT',
+             x_pot: str = 'Dirac',
+             batch_size: int = 256,
+             epochs: int = 100,
+             lr: float = 1E-5,
+             bool_load_params: bool = False):
+    #  scheduler_type: str = 'ones'):
+
+    mol_name = 'H2'
+    Ne = 2
+    coords = jnp.array([[0., 0., -1.4008538753/2], [0., 0., 1.4008538753/2]])
+    z = jnp.array([[1.], [1.]])
+    atoms = ['H', 'H']
+    mol = {'coords': coords, 'z': z}
+
+    png = jrnd.PRNGKey(0)
+    _, key = jrnd.split(png)
+
+    model_rev = CNF(3, (512, 512,), bool_neg=False)
+    model_fwd = CNF(3, (512, 512,), bool_neg=True)
+    test_inputs = lax.concatenate((jnp.ones((1, 3)), jnp.ones((1, 1))), 1)
+    params = model_rev.init(key, jnp.array(0.), test_inputs)
+
+    @jax.jit
+    def NODE_rev(params, batch): return neural_ode(
+        params, batch, model_rev, -1., 0., 3)
+
+    @jax.jit
+    def NODE_fwd(params, batch): return neural_ode(
+        params, batch, model_fwd, 0., 1., 3)
+
+    mean = jnp.zeros((3,))
+    cov = 3.*jnp.ones((3,))
+    prior_dist = MultivariateNormalDiag(mean, cov)
+
+    m = DFTDistribution(atoms, coords)
+    normalization_array = (m.coords, m.weights)
+
+    # optimizer = optax.adam(learning_rate=1E-3)
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(1.0),
+        optax.adam(learning_rate=lr),
+    )
+    opt_state = optimizer.init(params)
+
+    # load prev parameters
+    if bool_load_params:
+        restored_state = checkpoints.restore_checkpoint(
+            ckpt_dir=CKPT_DIR, target=params, step=0)
+        params = restored_state
+
+    @jax.jit
+    def rho_and_x(params, samples):
+        zt0 = samples[:, :-1]
+        zt, logp_zt = NODE_fwd(params, samples)
+        # logp_x = prior_dist.log_prob(zt0) + logp_zt
+        return jnp.exp(logp_zt), zt  # logp_x
+
+    @jax.jit
+    def rho_rev(params, x):
+        zt = lax.concatenate((x, jnp.zeros((x.shape[0], 1))), 1)
+        z0, logp_z0 = NODE_rev(params, zt)
+        logp_x = prior_dist.log_prob(z0)[:, None] - logp_z0
+        return jnp.exp(logp_x)  # logp_x
+
+    @jax.jit
+    def T(params, samples):
+        zt, _ = NODE_fwd(params, samples)
+        return zt
+
+    t_functional = _kinetic(t_kin)
+    v_functional = _nuclear(v_pot)
+    vh_functional = _hartree(h_pot)
+    x_functional = _exchange(x_pot)
+
+    @jax.jit
+    def loss(params, u_samples):
+        den_all, x_all = rho_and_x(params, u_samples)
+        den, denp = den_all[:batch_size], den_all[batch_size:]
+        x, xp = x_all[:batch_size], x_all[batch_size:]
+
+        e_t = t_functional(den, Ne)
+        e_h = vh_functional(x, xp, Ne)
+        e_nuc_v = v_functional(x, Ne, mol)
+        e_x = x_functional(den, Ne)
+
+        # cusp = cusp_condition(params, rho_rev, mol)
+        # cusp = 0.
+
+        e = e_t + e_nuc_v + e_h + 0.*e_x  # + cusp
+        energy = jnp.mean(e)
+        return energy, {"t": jnp.mean(e_t),
+                        "v": jnp.mean(e_nuc_v),
+                        "h": jnp.mean(e_h),
+                        "x": jnp.mean(e_x),
+                        "e": energy}
+        # "cusp": cusp}
+
+    @jax.jit
+    def step(params, opt_state, batch):
+        loss_value, grads = jax.value_and_grad(
+            loss, has_aux=True)(params, batch)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss_value
+
+    def batches_generator(key: prng.PRNGKeyArray, batch_size: int):
+        while True:
+            _, key = jrnd.split(key)
+            z0_and_logp_z0 = prior_dist.sample_and_log_prob(
+                seed=key, sample_shape=batch_size)
+            z0 = z0_and_logp_z0[0]
+            logp_z0 = z0_and_logp_z0[1][:, None]
+            samples0 = lax.concatenate((z0, logp_z0), 1)
+
+            _, key = jrnd.split(key)
+            z0_and_logp_z0 = prior_dist.sample_and_log_prob(
+                seed=key, sample_shape=batch_size)
+            z0 = z0_and_logp_z0[0]
+            logp_z0 = z0_and_logp_z0[1][:, None]
+            samples1 = lax.concatenate((z0, logp_z0), 1)
+
+            yield lax.concatenate((samples0, samples1), 0)
+
+    loss0 = jnp.inf
+    df = pd.DataFrame()
+    _, key = jrnd.split(key)
+    gen_batches = batches_generator(key, batch_size)
+    # scheduler = get_scheduler(epochs, scheduler_type)
+
+    for i in range(epochs+1):
+        # ci = scheduler(i)
+        batch = next(gen_batches)
+        params, opt_state, loss_value = step(params, opt_state, batch)  # , ci
+        loss_epoch, losses = loss_value
+
+        norm_val = compute_integral(
+            params, normalization_array, rho_rev, Ne, 0)
+
+        # norm_integral, log_det_Jac = _integral(params)
+        mean_energy = losses['e']
+        r_ = {'epoch': i,
+              'L': loss_epoch, 'E': mean_energy,
+              'T': losses['t'], 'V': losses['v'], 'H': losses['h'], 'X': losses['x'],
+              #   'cusp': losses['cusp'],
+              'I': norm_val,
+              # 'ci': ci
+              }
+        df = pd.concat([df, pd.DataFrame(r_, index=[0])], ignore_index=True)
+        df.to_csv(
+            f"{CKPT_DIR}/training_trajectory_{mol_name}.csv", index=False)
+
+        if i % 5 == 0:
+            _s = f"step {i}, L: {loss_epoch:.3f}, E:{mean_energy:.3f}\
+            T: {losses['t']:.5f}, V: {losses['v']:.5f}, H: {losses['h']:.5f}, X: {losses['x']:.5f}, I: {norm_val:.4f}"  # , cusp: {losses['cusp']:.5f}
+            print(_s,
+                  file=open(f"{CKPT_DIR}/loss_epochs_{mol_name}.txt", 'a'))
+
+        if loss_epoch < loss0:
+            params_opt, loss0 = params, loss_epoch
+            # checkpointing model model
+            checkpoints.save_checkpoint(
+                ckpt_dir=CKPT_DIR, target=params, step=0, overwrite=True)
+
+        if i % 20 == 0 or i <= 25:
+            xt = jnp.linspace(-4.5, 4.5, 1000)
+            yz = jnp.zeros((xt.shape[0], 2))
+            zt = lax.concatenate((yz, xt[:, None]), 1)
+            rho_pred = rho_rev(params, zt)
+
+            def model_identity(params, x): return x
+            def f_v(x): return v_functional(zt, Ne, mol)
+            v_pot = f_v(zt)
+
+            # exact density DFT
+            if i == 0:
+                rho_exact = m.prob(m, zt)
+                norm_dft = jnp.vdot(m.weights, m.prob(m, m.coords))
+
+            plt.figure(0)
+            plt.clf()
+            plt.title(
+                f'epoch {i}, L = {loss_epoch:.3f}, E = {mean_energy:.3f}, I = {norm_val:.3f}')  # , ci = {ci:.3f}
+            plt.plot(xt, rho_exact,
+                     color='k', ls=":", label=r"$\hat{\rho}_{DFT}(x)$, I = %.1f" % norm_dft)
+            plt.plot(xt, Ne*rho_pred,
+                     color='tab:blue', label=r'$N_{e}\;\rho_{NF}(x)$')
+            # plt.plot(xt, v_pot,
+            #          ls='--', color='k', label=r'$V(x)$')
+            plt.xlabel('x [Bhor]')
+            # plt.ylim(-5., 1.)
+            # plt.ylabel('Energy units')
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(f'{FIG_DIR}/epoch_{i}_rho.png')
+
+            # assert 0
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Density fitting training")
+    parser.add_argument("--epochs", type=int,
+                        default=500, help="training epochs")
+    parser.add_argument("--bs", type=int, default=512, help="batch size")
+    parser.add_argument("--params", type=bool, default=False,
+                        help="load pre-trained model")
+    parser.add_argument("--lr", type=float, default=3E-4,
+                        help="learning rate")
+    parser.add_argument("--kin", type=str, default='TF',
+                        help="Kinetic energy funcitonal")
+    parser.add_argument("--nuc", type=str, default='HGH',
+                        help="Nuclear Potential energy funcitonal")
+    parser.add_argument("--hart", type=str, default='MT',
+                        help="Hartree energy funcitonal")
+    parser.add_argument("--x", type=str, default='Dirac',
+                        help="Exchange energy funcitonal")
+    # parser.add_argument("--N", type=int, default=1, help="number of particles")
+    # parser.add_argument("--sched", type=str, default='one',
+    #                     help="Hartree integral scheduler")
+    # parser.add_argument("--sched", type=str, default='one',
+    #                     help="Hartree integral scheduler")
+    args = parser.parse_args()
+
+    batch_size = args.bs
+    epochs = args.epochs
+    bool_params = args.params
+    lr = args.lr
+
+    t = args.kin
+    v_pot = args.nuc
+    h_pot = args.hart
+    x_pot = args.x
+    # Ne = args.N
+    # scheduler_type = args.sched
+
+    global CKPT_DIR
+    global FIG_DIR
+    CKPT_DIR = f"Results/H2_{t}-{v_pot}-{h_pot}_new"
+    FIG_DIR = f"{CKPT_DIR}/Figures"
+
+    cwd = os.getcwd()
+    rwd = os.path.join(cwd, CKPT_DIR)
+    if not os.path.exists(rwd):
+        os.makedirs(rwd)
+    fwd = os.path.join(cwd, FIG_DIR)
+    if not os.path.exists(fwd):
+        os.makedirs(fwd)
+
+    training(t, v_pot, h_pot, x_pot, batch_size, epochs, lr)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ofdft_normflows/functionals_new.py
@@ -0,0 +1,355 @@
+from typing import Any, Callable
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+from jax import jit, vmap, hessian, jacrev, lax
+
+from ofdft_normflows.utils import *
+
+Array = jax.Array
+BHOR = 1.  # 1.8897259886  # 1AA to BHOR
+
+
+# ------------------------------------------------------------------------------------------------------------
+# KINETIC FUNCTIONALS
+# ------------------------------------------------------------------------------------------------------------
+
+def _kinetic(name: str = 'TF'):
+    if name.lower() == 'tf' or name.lower() == 'thomas_fermi':
+        def wrapper(*args):
+            return thomas_fermi(*args)
+    elif name.lower() == 'tf1d' or name.lower() == 'thomas_fermi_1d':
+        def wrapper(*args):
+            return thomas_fermi_1D(*args)
+    elif name.lower() == 'w' or name.lower() == 'weizsacker':
+        def wrapper(*args):
+            return weizsacker(*args)
+    elif name.lower() == 'k' or name.lower() == 'kinetic':
+        def wrapper(*args):
+            return kinetic(*args)
+    return wrapper
+
+
+# @ partial(jit,  static_argnums=(3,))
+@jit
+def kinetic(den: Any, lap_sqrt_den: Any, Ne: int) -> jax.Array:  # CHECK THIS ONE
+    rho_val = 1./(den+1E-4)**0.5  # for numerical stability
+    return -0.5*jnp.multiply(rho_val, lap_sqrt_den)
+
+
+# @partial(jit,  static_argnums=(3,))
+@jit
+def weizsacker(den: Array, grad_den: Array, Ne: int, l: Any = .2) -> jax.Array:
+    """
+    l = 0.2 (W Stich, EKU Gross., Physik A Atoms and Nuclei, 309(1):511, 1982.)
+    T_{\text{Weizsacker}}[\rho] &=& \frac{\lambda}{8} \int \frac{(\nabla \rho)^2}{\rho} dr = 
+                            &=&    \frac{\lambda}{8} \int  \rho \left(\frac{(\nabla \rho)}{\rho}\right)^2 dr\\
+    T_{\text{Weizsacker}}[\rho] = \mathbb{E}_{\rho} \left[ \left(\frac{(\nabla \rho)}{\rho}\right)^2 \right]
+
+    Args:
+        params (Any): _description_
+        den (Array): _description_
+        grad_den (Array): _description_
+        Ne (int): _description_
+        l (Any, optional): _description_. Defaults to .2.
+
+    Returns:
+        jax.Array: _description_
+    """
+
+    val = (grad_den/den)**2
+    return (l*Ne/8.)*val
+
+
+# @partial(jit,  static_argnums=(3,))
+@jit
+def thomas_fermi(den: Array, Ne: int) -> jax.Array:
+    """
+    T_{\text{TF}}[\rho] &=& \frac{3}{10}(3\pi^2)^{2/3} \int ( \rho)^{5/3} dr \\
+    T_{\text{TF}}[\rho] = \mathbb{E}_{\rho} \left[ ( \rho)^{2/3} \right]
+
+
+    Args:
+        params (Any): _description_
+        den (Array): _description_
+        Ne (int): _description_
+
+    Returns:
+        jax.Array: _description_
+    """
+
+    val = (den)**(2/3)
+    l = (3./10.)*(3.*jnp.pi**2)**(2/3)
+    return l*(Ne**(5/3))*val
+
+
+# @partial(jit,  static_argnums=(3,))
+@jit
+def thomas_fermi_1D(den: Array, Ne: int) -> jax.Array:
+    """
+    T_{\text{TF}}[\rho] &=& \frac{\pi^2}{12}\int ( \rho)^{3} dr \\
+    T_{\text{TF}}[\rho] = \frac{\pi^2}{12}\mathbb{E}_{\rho} \left[ ( \rho)^{2} \right]
+
+    Args:
+        params (Any): _description_
+        u (Array): _description_
+        Ne (int): _description_
+        fun (callable): _description_
+
+    Returns:
+        jax.Array: _description_
+    """
+
+    den_sqr = den*den
+    l = (jnp.pi*jnp.pi)/12.
+    return l*(Ne**3)*den_sqr
+# ------------------------------------------------------------------------------------------------------------
+
+
+def _exchange(name: str = 'dirac'):
+    if name.lower() == 'dirac':
+        def wrapper(*args):
+            return Dirac_exchange(*args)
+    return wrapper
+
+
+@jit
+def Dirac_exchange(den: Array, Ne: int) -> jax.Array:
+    """
+    ^{Dirac}E_{\text{x}}[\rho] = -\frac{3}{4}\left(\frac{3}{\pi}\right)^{1/3}\int  \rho^{4/3} dr \\
+    ^{Dirac}E_{\text{x}}[\rho] = -\frac{3}{4}\left(\frac{3}{\pi}\right)^{1/3}\mathbb{E}_{\rho} \left[ \rho^{1/3} \right]
+
+    Args:
+        params (Any): _description_
+        den (Array): _description_
+        Ne (int): _description_
+
+    Returns:
+        jax.Array: _description_
+    """
+    l = -(3/4)*(Ne**(4/3))*(3/jnp.pi)**1/3
+    return l*den**(1/3)
+
+
+# ------------------------------------------------------------------------------------------------------------
+
+
+def _hartree(name: str = 'mt'):
+    if name.lower() == 'mt':
+        def wrapper(*args):
+            return Hartree_potential_MT(*args)
+    else:  # full
+        def wrapper(*args):
+            return Hartree_potential(*args)
+
+    return wrapper
+
+
+# @partial(jax.jit,  static_argnums=(4, 5, ))
+@jit
+def Hartree_potential(x: Any, xp: Any, Ne: int, eps=1E-5):
+    z = jnp.sum((x-xp)*(x-xp)+eps, axis=-1, keepdims=True)
+    z = 1./(z**0.5)
+    return 0.5*(Ne**2)*z
+
+
+@jit
+def Hartree_potential_MT(x: Any, xp: Any, Ne: int, alpha=0.5):
+    # Martyna-Tuckerman J. Chem. Phys. 110, 2810–2821 (1999), Eq. B1, alpha_conv * L > 7
+    # alpha_conv * L = 5, L = 10 A -> alpha_conv = 0.9448623 (Table 1 of J. Chem. Phys. 110, 2810–2821 (1999))
+
+    r = jnp.sum((x-xp)*(x-xp), axis=-1, keepdims=True)
+    r = jnp.sqrt(r)
+    return 0.5*(Ne**2)*(lax.erf(alpha*r)/r + lax.erfc(alpha*r)/r)
+
+
+# ------------------------------------------------------------------------------------------------------------
+# POTENTIAL FUNCTIONALS
+# ------------------------------------------------------------------------------------------------------------
+
+
+def _nuclear(name: str = 'HGH'):
+    if name.lower() == 'hgh':
+        def wrapper(*args):
+            return Nuclei_potential_HGH(*args)
+    elif name.lower() == 'madness':
+        def wrapper(*args):
+            return Nuclei_potential_smooth(*args)
+    elif name.lower() == 'harmonic' or name.lower() == 'ho':
+        def wrapper(*args):
+            return Nuclei_potential_smooth(*args)
+    else:
+        def wrapper(*args):
+            return Nuclei_potential(*args)
+
+    return wrapper
+
+
+@jit
+def harmonic_potential(params: Any, x: Any, Ne: int, k: Any = 1.) -> jax.Array:
+    return 0.5*Ne*k*jnp.mean(x**2)
+
+
+@partial(jax.jit,  static_argnums=(3,))
+def Nuclei_potential(params: Any, u: Any, Ne: int, T: Callable, mol_info: Any):
+    eps = 1E-4  # 0.2162
+
+    @jit
+    def _potential(x: Any, molecule: Any):
+        r = jnp.sqrt(
+            jnp.sum((x-molecule['coords'])*(x-molecule['coords']), axis=1)) + eps
+        z = molecule['z']
+        return z/r
+
+    x = T(params, u)
+    r = vmap(_potential, in_axes=(None, 0), out_axes=-1)(x, mol_info)
+    r = jnp.sum(r, axis=-1, keepdims=True)
+    return -Ne*r  # lax.expand_dims(r, dimensions=(1,))
+
+
+@jit
+def Nuclei_potential_smooth(params: Any, x: Any,  Ne: int, mol_info: Any):
+    # J. Chem. Phys. 121, 11587–11598 (2004)
+    # Eq 25-27
+    eps = 1E-2  # 0.2162
+    c0 = 0.00435
+    pi_sqrt = jnp.sqrt(jnp.pi)
+
+    @jax.jit
+    def _u(r: Any):
+        r2 = r*r
+        return lax.erf(r)/r + (1/(3*pi_sqrt))*(jnp.exp(-r2)+16*jnp.exp(-4*r2))
+
+    @jax.jit
+    def _potential(x: Any, molecule: Any):
+        z = molecule['z']
+        r = jnp.sqrt(
+            jnp.sum((x-molecule['coords'])*(x-molecule['coords']), axis=1))
+        c = (c0*eps/z**5)**1.3
+        v = _u(r/c)/c
+        return v
+
+    r = vmap(_potential, in_axes=(None, 0), out_axes=-1)(x, mol_info)
+    r = jnp.sum(r, axis=-1, keepdims=True)
+    return -Ne*r  # lax.expand_dims(r, dimensions=(1,))
+
+
+@jit
+def Nuclei_potential_HGH(x: Any,  Ne: int, mol_info: Any):
+    # INCORRECT only Hydrogen parameters
+    #  Phys. Rev. B 58, 3641
+    two_sqrt = jnp.sqrt(2)
+    # H_pp_params = {'Zion': jnp.ones(1), 'rloc': 2*jnp.ones(1),
+    #                'C1': -4.180237*jnp.ones(1), 'C2': 0.725075*jnp.ones(1), 'C3': jnp.zeros(1), 'C4': jnp.zeros(1), }
+    H_pp_params = {'Zion': 1., 'rloc': 0.2,
+                   'C1': -4.180237, 'C2': 0.725075, 'C3': 0., 'C4': 0., }
+
+    @jax.jit
+    def _u(r: Any, params_pp: Any):
+        # eq 1
+        zion = params_pp['Zion']
+        rloc = params_pp['rloc']
+        c1 = params_pp['C1']
+        c2 = params_pp['C2']
+        c3 = params_pp['C3']
+        c4 = params_pp['C4']
+        r_rloc = r/rloc
+        r_rloc_2 = r_rloc*r_rloc
+        v0 = (-zion/r)*lax.erf(r_rloc/two_sqrt)
+        v1 = jnp.exp(-0.5*(r_rloc_2))
+        v2 = c1 + c2*r_rloc_2 + c3*(r_rloc**4) + c4*(r_rloc**6)
+        return v0 + v1*v2
+
+    @jax.jit
+    def _potential(x: Any, molecule: Any):
+        z = molecule['z']
+        # ai = molecule['atoms']
+        r = jnp.sqrt(
+            jnp.sum((x-molecule['coords'])*(x-molecule['coords']), axis=1))
+        params_p_zi = H_pp_params
+        v = _u(r, params_p_zi)
+        return v
+
+    r_all = vmap(_potential, in_axes=(None, 0), out_axes=-1)(x, mol_info)
+    r = jnp.sum(r_all, axis=-1, keepdims=True)
+    return Ne*r  # lax.expand_dims(r, dimensions=(1,))
+
+
+@jit
+def GaussianPotential1D(x: Any, Ne: int, params_pot: Any = None) -> jax.Array:
+    if (params_pot is None):
+        params_pot = {'alpha': jnp.array([[1.], [2.]]),  # Ha/electron
+                      'beta': -1.*jnp.array([[-0.5], [1.]])}  # BHOR
+
+    @jit
+    def _f(x: Array, params_pot: Any):
+        alpha, beta = params_pot['alpha'], params_pot['beta']
+        return -alpha*jnp.exp(-(x-beta)*(x-beta))  # **2 OLD
+
+    y = vmap(_f, in_axes=(None, 1))(x, params_pot)
+    y = jnp.sum(y, axis=-1).transpose()
+    return Ne*y
+
+
+@partial(jax.jit,  static_argnums=(1,))
+def cusp_condition(params: Any, fun: callable, mol_info: Any):
+
+    @jax.jit
+    def _cusp(molecule: Any):
+        x = molecule['coords'][None]
+        z = molecule['z']
+        rho_val = fun(params, x)
+        d_rho_val = score(params, x, fun)
+        # check with the norm
+        return jnp.sum(d_rho_val) - (-2*z*jnp.sum(rho_val))
+
+    l = vmap(_cusp)(mol_info)
+    return jnp.mean(jnp.abs(l))
+
+
+if __name__ == '__main__':
+
+    coords = jnp.array([[0., 0., -1.4008538753/2], [0., 0., 1.4008538753/2]])
+    z = jnp.array([[1], [1]], dtype=int)
+    # atoms = jnp.array(['H', 'H'], dtype=str)
+    mol = {'coords': coords, 'z': z}  # 'atoms': atoms
+
+    rng = jax.random.PRNGKey(0)
+    _, key = jax.random.split(rng)
+    x = jax.random.uniform(key, shape=(10, 3))
+    _, key = jax.random.split(key)
+    xp = jax.random.uniform(key, shape=(10, 3))
+    def model_identity(params, x): return x
+    y = Nuclei_potential(None, x, model_identity, mol)
+    # print(y.shape)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    xt = jnp.linspace(-1.5, 1.5, 500)
+    yz = jnp.zeros((xt.shape[0], 2))
+    xyz = lax.concatenate((yz, xt[:, None]), 1)
+    v_pot = y = Nuclei_potential(None, xyz, model_identity, mol)
+    v_pot_s = Nuclei_potential_smooth(None, xyz, model_identity, mol)
+    v_pot_hgh = Nuclei_potential_HGH(None, xyz, model_identity, mol)
+
+    from dft_distrax import DFTDistribution
+    atoms = ['H', 'H']
+    geom = coords
+
+    m = DFTDistribution(atoms, geom)
+
+    x = jnp.ones((10, 3))
+    # print(m.prob(m,geom))
+    rho = m.prob(m, xyz)
+    # def log_prob(value):
+    #     return jnp.log(m.prob(m, value))
+
+    # plt.plot(xt, rho, ls='--', color='k')
+    plt.plot(xt, v_pot)
+    plt.scatter(xt, v_pot_hgh, s=5)
+    # plt.scatter(xt, v_pot_s, s=5)
+    plt.ylim(bottom=-25., top=-1)
+    plt.show()
+
+    v_h = Hartree_potential_MT(None, x, xp, model_identity)
+    print(v_h.shape)
