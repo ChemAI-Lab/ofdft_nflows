9a5b1e37ca01458947453ed9f5ff41240f09018b
RodrigoAVargasHdz <32406514+RodrigoAVargasHdz@users.noreply.github.com>
2023-08-04 11:14:12 -0400

    score compute with ODE

--- ofdft_normflows/cn_flows.py
+++ ofdft_normflows/cn_flows.py
@@ -11,6 +11,7 @@ from flax import linen as nn
 import optax
 
 # file from https://huggingface.co/flax-community/NeuralODE_SDE/raw/main/train_cnf.py
+# jax.config.update('jax_disable_jit', True)
 
 a_initializer = jax.nn.initializers.normal()
 
@@ -124,9 +125,9 @@ class SimpleMLP(nn.Module):
         self.layers = [nn.Dense(feat)
                        for feat in self.features]  # [1:]
         self.last_layer = nn.Dense(
-            self.in_out_dims)  # ,
-        # kernel_init=jax.nn.initializers.zeros,
-        # bias_init=jax.nn.initializers.zeros)
+            self.in_out_dims,
+            kernel_init=jax.nn.initializers.zeros,
+            bias_init=jax.nn.initializers.zeros)
         # kernel_init=jax.nn.initializers.constant(1E-2),
         # bias_init=jax.nn.initializers.constant(1E-2))
 
@@ -189,7 +190,7 @@ class Gen_CNFSimpleMLP(nn.Module):
         return self.y0 * outputs
 
 
-@partial(jax.jit,  static_argnums=(2, 3, 4, 5,))
+# @partial(jax.jit,  static_argnums=(2, 3, 4, 5,))
 def neural_ode(params: Any, batch: Any, f: Callable, t0: float, t1: float, d_dim: int):
     # time as [t1 to t0] gives nans for the second term :/
     start_and_end_time = jnp.array([t0, t1])
@@ -213,20 +214,126 @@ def neural_ode(params: Any, batch: Any, f: Callable, t0: float, t1: float, d_dim
     # return outputs
 
 
+@partial(jax.jit,  static_argnums=(2, 3, 4, 5,))
+def neural_ode_score(params: Any, batch: Any, f: Callable, t0: float, t1: float, d_dim: int):
+    # time as [t1 to t0] gives nans for the second term :/
+    start_and_end_time = jnp.array([t0, t1])
+
+    @jax.jit
+    def _evol_fn_i(params, t, state):
+        state = lax.expand_dims(state, dimensions=(0,))
+        state, score = state[:, :-in_out_dim], state[:, -in_out_dim:]
+        def _f_div(state): return jnp.sum(
+            f.apply(params, t, state)[:, -1:])
+
+        def _f_dx(state): return f.apply(params, t, state)[:, :-1]
+
+        div, grad_div = jax.value_and_grad(_f_div)(state[:, :-1])
+        dx, _f_vjp = vjp(_f_dx, state[:, :-1])
+        score_vjp = _f_vjp(score)[0]
+        dscore = -score_vjp+grad_div  # check if the +grad_div
+        state = lax.concatenate(
+            (dx, lax.expand_dims(div, dimensions=(0, 1)), dscore), 1)
+        return state.ravel()
+    v_evol_fn_i = vmap(_evol_fn_i, in_axes=(None, None, 0), out_axes=(0))
+
+    def _evol_fun(states, t):
+        return v_evol_fn_i(params, t, states)
+
+    outputs = odeint(
+        _evol_fun,
+        batch,
+        start_and_end_time,
+        atol=1e-5,
+        rtol=1e-5
+    )
+    z_t, logp_diff_t, score_t = outputs[:, :,
+                                        :d_dim], outputs[:, :, d_dim:d_dim+1], outputs[:, :, d_dim+1:]
+    # z_t0, logp_diff_t0 = z_t[0], logp_diff_t[0]
+    z_t1, logp_diff_t1, score_t1 = z_t[-1], logp_diff_t[-1], score_t[-1]
+    # return lax.concatenate((z_t0, z_t1), 2), lax.concatenate((lax.expand_dims(logp_diff_t0, (1,)), lax.expand_dims(logp_diff_t1, (1,))), 1)
+    return z_t1, logp_diff_t1, score_t1
+    # return outputs
+
+
 if __name__ == '__main__':
     import jax.random as jrnd
     from jax.tree_util import tree_map
+    from jax import value_and_grad, vjp
+
+    import jax.random as jrnd
+    import distrax
+    from distrax import Normal, MultivariateNormalDiag
+
     rng = jrnd.PRNGKey(0)
     _, key = jrnd.split(rng)
 
-    model = FourierFeatures(10)
-    params = model.init(key, jnp.ones((1, 3)))
-    print(params)
-    model = Gen_CNFSimpleMLP(3, [10, 10])
-    params = model.init(key, jnp.array(0.), jnp.array([[1., 1., 1., 1.]]))
-    print(params)
-    # params_bool = tree_map(lambda x: params.shape, params)
+    in_out_dim = 1
 
-    # # print(model.apply(params, jnp.array(0.), jnp.array([[1.,  1.]])))
-    # x = jnp.zeros((10, 4))
-    # print(model.apply(params, jnp.array(0.), x))
+    # dist_prior = Normal(0., 1.)
+    mean = jnp.zeros((in_out_dim,))
+    cov = jnp.ones((in_out_dim,))
+    prior_dist = MultivariateNormalDiag(mean, cov)
+    x0 = prior_dist.sample(seed=key, sample_shape=(4,))
+    log_prob = prior_dist.log_prob(x0)
+    score = vmap(jax.jacrev(lambda x:
+                            prior_dist.log_prob(x)))(x0)
+    x0 = lax.concatenate((x0, log_prob[:, None], score), 1)
+    print(x0)
+
+    # init model
+    model_rev = Gen_CNFSimpleMLP(in_out_dim, (512, 512,), bool_neg=False)
+    model_fwd = Gen_CNFSimpleMLP(in_out_dim, (512, 512,), bool_neg=True)
+    test_inputs = x_test = jnp.ones((1, in_out_dim+1+score.shape[1]))
+    t = jnp.array(0.)
+    params = model_fwd.init(key, jnp.array(0.), test_inputs)
+
+    @jax.jit
+    def NODE_fwd_w_score(params, batch): return neural_ode_score(
+        params, batch, model_fwd, 0., 1., in_out_dim)
+
+    # log prob
+    # @jax.jit
+    def log_prob_wscore(params, samples):
+        zt, logp_zt, score = NODE_fwd_w_score(params, samples)
+        return logp_zt, (zt, score)
+
+    logp, (x, score) = log_prob_wscore(params, x0)
+    print('ODE score')
+    print('x:', x)
+    print('logp:', logp)
+    print('score:', score)
+
+    # assert 0
+   # AD SCORE
+    print('AD score')
+
+    @jax.jit
+    def NODE_fwd(params, batch): return neural_ode(
+        params, batch, model_fwd, 0., 1., in_out_dim)
+
+    def log_prob(params, samples):
+        zt, logp_zt = NODE_fwd(params, samples[None])
+        return jnp.sum(logp_zt), zt.ravel()
+
+    x0 = x0[:, :-in_out_dim]
+    logp, (x, outputs) = vmap(log_prob, in_axes=(None, 0))(params, x0)
+    # print(logp)
+    print('x:', x)
+    print('logp:', logp)
+
+    score_ad, _ = vmap(jax.grad(log_prob, argnums=(
+        1,), has_aux=True), in_axes=(None, 0))(params, x0)
+    print('x:', x)
+    print('logp:', logp)
+    print('score:', score_ad)
+
+    # EXTRA
+    '''
+    def gf(params, t, state):
+        div_and_dx, g = value_and_grad(
+            f, argnums=(2,), has_aux=True)(params, t, state)
+        div, dx = div_and_dx
+        grad_div = g[0][:, :in_out_dim]
+        return div, dx, (div, dx, grad_div)    
+    '''
