4d8ba12b4dd38447a55933aabfb26ffcceb26db9
RodrigoAVargasHdz <32406514+RodrigoAVargasHdz@users.noreply.github.com>
2023-08-25 18:00:42 -0400

    h2o

--- h2o_mol_ofdft_min.py
+++ h2o_mol_ofdft_min.py
@@ -10,8 +10,10 @@ from jax import lax, vmap, numpy as jnp
 import jax.random as jrnd
 from jax._src import prng
 
+import chex
 from flax.training import checkpoints
 import optax
+from optax import ema
 from distrax import MultivariateNormalDiag, Laplace
 
 from ofdft_normflows.functionals import _kinetic, _nuclear, _hartree, _exchange, cusp_condition
@@ -19,7 +21,7 @@ from ofdft_normflows.dft_distrax import DFTDistribution
 from ofdft_normflows.cn_flows import neural_ode, neural_ode_score
 from ofdft_normflows.cn_flows import Gen_CNFSimpleMLP as CNF
 from ofdft_normflows.cn_flows import Gen_CNFRicky as CNFRicky
-from ofdft_normflows.utils import get_scheduler
+from ofdft_normflows.utils import get_scheduler, batches_generator_w_score
 
 import matplotlib.pyplot as plt
 
@@ -41,6 +43,15 @@ def compute_integral(params: Any, grid_array: Any, rho: Any, Ne: int, bs: int):
     return jnp.vdot(grid_weights, rho_val)
 
 
+@chex.dataclass
+class F_values:
+    energy: chex.ArrayDevice
+    kin: chex.ArrayDevice
+    vnuc: chex.ArrayDevice
+    hart: chex.ArrayDevice
+    x: chex.ArrayDevice
+
+
 def training(t_kin: str = 'TF',
              v_pot: str = 'HGH',
              h_pot: str = 'MT',
@@ -51,6 +62,8 @@ def training(t_kin: str = 'TF',
              nn_arch: tuple = (512, 512,),
              bool_load_params: bool = False,
              scheduler_type: str = 'ones'):
+
+    CKPT_DIR_ALL = f"{CKPT_DIR}/checkpoints_all/"
 
     mol_name = 'H2O'
     Ne = 10
@@ -86,9 +99,9 @@ def training(t_kin: str = 'TF',
 
     mean = jnp.zeros((3,))
     cov = jnp.ones((3,))
-    prior_dist = MultivariateNormalDiag(mean, cov)
+    prior_dist = MultivariateNormalDiag(mean, cov,)
 
-    m = DFTDistribution(atoms, coords)
+    m = DFTDistribution(atoms=atoms, geometry=coords, geom_units='B')
     normalization_array = (m.coords, m.weights)
 
     # optimizer = optax.adam(learning_rate=1E-3)
@@ -99,6 +112,9 @@ def training(t_kin: str = 'TF',
         # optax.adam(learning_rate=lr_sched),
     )
     opt_state = optimizer.init(params)
+    energies_ema = ema(decay=0.99)
+    energies_state = energies_ema.init(
+        F_values(energy=jnp.array(0.), kin=jnp.array(0.), vnuc=jnp.array(0.), hart=jnp.array(0.), x=jnp.array(0.)))
 
     # load prev parameters
     if bool_load_params:
@@ -147,17 +163,14 @@ def training(t_kin: str = 'TF',
         e_nuc_v = v_functional(x, Ne, mol)
         e_x = x_functional(den, Ne)
 
-        # cusp = cusp_condition(params, rho_rev, mol)
-        # cusp = 0.
-
-        e = e_t + e_nuc_v + e_h + e_x  # + cusp
+        e = e_t + e_nuc_v + e_h + e_x
         energy = jnp.mean(e)
-        return energy, {"t": jnp.mean(e_t),
-                        "v": jnp.mean(e_nuc_v),
-                        "h": jnp.mean(e_h),
-                        "x": jnp.mean(e_x),
-                        "e": energy}
-        # "cusp": cusp}
+        f_values = F_values(energy=energy,
+                            kin=jnp.mean(e_t),
+                            vnuc=jnp.mean(e_nuc_v),
+                            hart=jnp.mean(e_h),
+                            x=jnp.mean(e_x))
+        return energy, f_values
 
     @jax.jit
     def step(params, opt_state, batch):
@@ -167,130 +180,64 @@ def training(t_kin: str = 'TF',
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss_value
 
-    def batches_generator(key: prng.PRNGKeyArray, batch_size: int):
-        v_score = vmap(jax.jacrev(lambda x:
-                                  prior_dist.log_prob(x)))
-        while True:
-            _, key = jrnd.split(key)
-            samples = prior_dist.sample(seed=key, sample_shape=batch_size)
-            logp_samples = prior_dist.log_prob(samples)
-            score = v_score(samples)
-            samples0 = lax.concatenate(
-                (samples, logp_samples[:, None], score), 1)
-
-            _, key = jrnd.split(key)
-            samples = prior_dist.sample(seed=key, sample_shape=batch_size)
-            logp_samples = prior_dist.log_prob(samples)
-            score = v_score(samples)
-            samples1 = lax.concatenate(
-                (samples, logp_samples[:, None], score), 1)
-
-            yield lax.concatenate((samples0, samples1), 0)
-
-    loss0 = jnp.inf
-    df = pd.DataFrame()
     _, key = jrnd.split(key)
-    gen_batches = batches_generator(key, batch_size)
+    gen_batches = batches_generator_w_score(key, batch_size, prior_dist)
 
+    df = pd.DataFrame()
+    df_ema = pd.DataFrame()
     for i in range(epochs+1):
-        # ci = scheduler(i)
         batch = next(gen_batches)
         params, opt_state, loss_value = step(params, opt_state, batch)  # , ci
         loss_epoch, losses = loss_value
 
+        # functionals values ema
+        energies_i_ema, energies_state = energies_ema.update(
+            losses, energies_state)
+        ei_ema = energies_i_ema.energy
         norm_val = compute_integral(
             params, normalization_array, rho_rev, Ne, 0)
 
-        # norm_integral, log_det_Jac = _integral(params)
-        mean_energy = losses['e']
         r_ = {'epoch': i,
-              'L': loss_epoch, 'E': mean_energy,
-              'T': losses['t'], 'V': losses['v'], 'H': losses['h'], 'X': losses['x'],
-              #   'cusp': losses['cusp'],
+              'E': loss_epoch,
+              'T': losses.kin, 'V': losses.vnuc, 'H': losses.hart, 'X': losses.x,
               'I': norm_val,
-              # 'ci': ci
               }
+
         df = pd.concat([df, pd.DataFrame(r_, index=[0])], ignore_index=True)
         df.to_csv(
             f"{CKPT_DIR}/training_trajectory_{mol_name}.csv", index=False)
 
-        if i % 5 == 0:
-            _s = f"step {i}, L: {loss_epoch:.3f}, E:{mean_energy:.3f}\
-            T: {losses['t']:.5f}, V: {losses['v']:.5f}, H: {losses['h']:.5f}, X: {losses['x']:.5f}, I: {norm_val:.4f}"  # , cusp: {losses['cusp']:.5f}
-            print(_s,
-                  file=open(f"{CKPT_DIR}/loss_epochs_{mol_name}.txt", 'a'))
+        r_ema = {'epoch': i,
+                 'E': energies_i_ema.energy,
+                 'T': energies_i_ema.kin, 'V': energies_i_ema.vnuc, 'H': energies_i_ema.hart, 'X': energies_i_ema.x,
+                 'I': norm_val,
+                 }
+        df_ema = pd.concat(
+            [df_ema, pd.DataFrame(r_ema, index=[0])], ignore_index=True)
+        df_ema.to_csv(
+            f"{CKPT_DIR}/training_trajectory_{mol_name}_ema.csv", index=False)
 
-        if loss_epoch < loss0:
-            params_opt, loss0 = params, loss_epoch
-            # checkpointing model model
-            checkpoints.save_checkpoint(
-                ckpt_dir=CKPT_DIR, target=params, step=0, overwrite=True)
-            bool_plot = True
-        else:
-            bool_plot = False
-
-        if i % 10 == 0 or i <= 50 or bool_plot:
-            # 2D figure
-            z = jnp.linspace(-5.25, 5.25, 128)
-            x = 0.  # 0.699199  # H covalent radius
-            yy, zz = jnp.meshgrid(z, z)
-            X = jnp.array(
-                [x*jnp.ones_like(yy.ravel()), yy.ravel(),  zz.ravel()]).T
-            rho_pred = Ne*rho_rev(params, X)
-
-            # exact density DFT
-            if i == 0:
-                rho_exact_2D = m.prob(m, X)
-
-            vmin = jnp.min(jnp.stack([rho_exact_2D.ravel(), rho_pred.ravel()]))
-            vmax = jnp.max(jnp.stack([rho_exact_2D.ravel(), rho_pred.ravel()]))
-            # fig, ax = plt.subplots()
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-            # plt.clf()
-            ax1.set_title(
-                f'epoch {i}, L = {loss_epoch:.3f}, E = {mean_energy:.3f}, I = {norm_val:.3f}')
-            contour1 = ax1.contourf(
-                yy, zz, rho_pred.reshape(yy.shape), levels=25,  vmin=vmin, vmax=vmax)
-            # label=r'$N_{e}\;\rho_{NF}(x)$')
-            cbar = fig.colorbar(contour1, ax=ax1)
-            cbar.set_label(r'$N_{e}\rho_{NF}(x)$')
-
-            contour2 = ax2.contourf(yy, zz, rho_exact_2D.reshape(xx.shape),
-                                    linestyles='dashed', levels=25, vmin=vmin, vmax=vmax)
-            #    label=r"$\hat{\rho}_{DFT}(x)$")
-
-            cbar = fig.colorbar(contour2, ax=ax2)
-            cbar.set_label(r'$\rho(x)$')
-
-            ax1.scatter(coords[:, 1], coords[:, 2],
-                        marker='o', color='k', s=35, zorder=2.5)
-            ax2.scatter(coords[:, 1], coords[:, 2],
-                        marker='o', color='k', s=35, zorder=2.5)
-            ax1.set_xlabel('Y [Bhor]')
-            ax1.set_ylabel('Z [Bhor]')
-            ax2.set_xlabel('Y [Bhor]')
-            ax2.set_ylabel('Z [Bhor]')
-            # plt.legend()
-            plt.tight_layout()
-            plt.savefig(f'{FIG_DIR}/epoch_rho_xz_{i}.png')
+        # save models
+        checkpoints.save_checkpoint(
+            ckpt_dir=CKPT_DIR_ALL, target=params, step=i, keep_every_n_steps=10)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Density fitting training")
     parser.add_argument("--epochs", type=int,
-                        default=1, help="training epochs")
-    parser.add_argument("--bs", type=int, default=12, help="batch size")
+                        default=1000, help="training epochs")
+    parser.add_argument("--bs", type=int, default=512, help="batch size")
     parser.add_argument("--params", type=bool, default=False,
                         help="load pre-trained model")
     parser.add_argument("--lr", type=float, default=3E-4,
                         help="learning rate")
     parser.add_argument("--kin", type=str, default='tf-w',
                         help="Kinetic energy funcitonal")
-    parser.add_argument("--nuc", type=str, default='HGH',
+    parser.add_argument("--nuc", type=str, default='V',
                         help="Nuclear Potential energy funcitonal")
-    parser.add_argument("--hart", type=str, default='MT',
+    parser.add_argument("--hart", type=str, default='H',
                         help="Hartree energy funcitonal")
-    parser.add_argument("--x", type=str, default='Dirac',
+    parser.add_argument("--x", type=str, default='X',
                         help="Exchange energy funcitonal")
     # parser.add_argument("--N", type=int, default=1, help="number of particles")
     parser.add_argument("--sched", type=str, default='const',
@@ -316,6 +263,8 @@ def main():
     global CKPT_DIR
     global FIG_DIR
     CKPT_DIR = f"Results/H2O_{kin.upper()}_{v_pot.upper()}_{h_pot.upper()}_{x_pot.upper()}_lr_{lr:.1e}"
+    if sched_type.lower() != 'c' or sched_type.lower() != 'const':
+        CKPT_DIR = CKPT_DIR + f"_sched_{sched_type.upper()}"
     FIG_DIR = f"{CKPT_DIR}/Figures"
 
     cwd = os.getcwd()
